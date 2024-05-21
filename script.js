1238c5b81c948acda7e134b74a51287336dffdff
Incultavulpes <fernando.revenga@gmail.com>
2024-05-21 21:06:21 +0200

    Do exercise

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let first;
 let second;
 let result = 0;
 let operator;
+let operationKeysPressedState = false;
 
 function add (first, second) {
     return first + second;
@@ -31,6 +32,116 @@ ac.addEventListener("click", () => display.textContent = "");
 
 let naught = document.querySelector(".naught");
 naught.addEventListener("click", () => display.textContent = display.textContent + "0");
+
+let one = document.querySelector(".one");
+one.addEventListener("click", () => {
+    display.textContent = display.textContent + "1";
+    if (operationKeysPressedState) {
+        display.textContent = "";
+        display.textContent = display.textContent + "1";
+        operationKeysPressedState = false;
+    }
+});
+
+let two = document.querySelector(".two");
+two.addEventListener("click", () => {
+    display.textContent = display.textContent + "2";
+    if (operationKeysPressedState) {
+        display.textContent = "";
+        display.textContent = display.textContent + "2";
+        operationKeysPressedState = false;
+    }
+});
+
+let three = document.querySelector(".three");
+three.addEventListener("click", () => {
+    display.textContent = display.textContent + "3";
+    if (operationKeysPressedState) {
+        display.textContent = "";
+        display.textContent = display.textContent + "3";
+        operationKeysPressedState = false;
+    }
+});
+
+let four = document.querySelector(".four");
+four.addEventListener("click", () => {
+    display.textContent = display.textContent + "4";
+    if (operationKeysPressedState) {
+        display.textContent = "";
+        display.textContent = display.textContent + "4";
+        operationKeysPressedState = false;
+    }
+});
+
+let five = document.querySelector(".five");
+five.addEventListener("click", () => {
+    display.textContent = display.textContent + "5";
+    if (operationKeysPressedState) {
+        display.textContent = "";
+        display.textContent = display.textContent + "5";
+        operationKeysPressedState = false;
+    }
+});
+
+let six = document.querySelector(".six");
+six.addEventListener("click", () => {
+    display.textContent = display.textContent + "6";
+    if (operationKeysPressedState) {
+        display.textContent = "";
+        display.textContent = display.textContent + "6";
+        operationKeysPressedState = false;
+    }
+});
+
+let seven = document.querySelector(".seven");
+seven.addEventListener("click", () => {
+    display.textContent = display.textContent + "7";
+    if (operationKeysPressedState) {
+        display.textContent = "";
+        display.textContent = display.textContent + "7";
+        operationKeysPressedState = false;
+    }
+});
+
+let eight = document.querySelector(".eight");
+eight.addEventListener("click", () => {
+    display.textContent = display.textContent + "8";
+    if (operationKeysPressedState) {
+        display.textContent = "";
+        display.textContent = display.textContent + "8";
+        operationKeysPressedState = false;
+    }
+});
+
+let nine = document.querySelector(".nine");
+nine.addEventListener("click", () => {
+    display.textContent = display.textContent + "9";
+    if (operationKeysPressedState) {
+        display.textContent = "";
+        display.textContent = display.textContent + "9";
+        operationKeysPressedState = false;
+    }
+});
+
+let dot = document.querySelector(".dot");
+dot.addEventListener("click", () => display.textContent = display.textContent + ".");
+
+let addition = document.querySelector(".additioning");
+addition.addEventListener("click", () => {
+    first = display.textContent;
+    first = +first;
+    operator = add;
+    console.log("Hey dude");
+    operationKeysPressedState = true;
+});
+
+let equal = document.querySelector(".equalizating");
+equal.addEventListener("click", () => {
+    second = display.textContent;
+    second = +second;
+    result = operate(operator, first, second);
+    display.textContent = result.toString();
+});
 
 /* Lo que vamos a añadir en las próximas sesiones de programación y cómo hacerlo:
         1.- El CSS no está realmente acabado, tenemos que modificar el cursor tipo de texto y evitar que el subrayado del texto ocurra.
